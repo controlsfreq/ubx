9bad59bbf52f8549be08849d891c29ad924708b3
controlsfreq <controls.freq@gmail.com>
2016-02-06 12:49:18 -0800

    Fix project name
    * Remove non-ascii characters from Sphinx project name.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -51,7 +51,7 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = 'Eärendil'
+project = 'Earendil'
 copyright = '2016, Liam Bucci'
 author = 'Liam Bucci'
 
